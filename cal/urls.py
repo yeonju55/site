975ea696bc96bfd921acc71b424b31d75444125a
Yeonju Kang <ohyeon412@hanyang.ac.kr>
2023-02-05 18:18:51 +0900

    Initial commit

---
+++ cal/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'cal'
+
+urlpatterns = [
+    url(r'^index/$', views.index, name='index'),
+    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'), # here
+]
