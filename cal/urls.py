22cbc2d68230e50216e7c316dd6b24c987b64b7c
Yeonju Kang <ohyeon412@hanyang.ac.kr>
2023-02-05 22:17:23 +0900

    calendar에 일정 추가

--- cal/urls.py
+++ cal/urls.py
@@ -6,4 +6,6 @@ app_name = 'cal'
 urlpatterns = [
     url(r'^index/$', views.index, name='index'),
     url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'), # here
+    url(r'^event/new/$', views.event, name='event_new'),
+    url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
 ]
--- cal/views.py
+++ cal/views.py
@@ -28,8 +28,40 @@ class CalendarView(generic.ListView):
         context['calendar'] = mark_safe(html_cal)
         return context
 
+        d = get_date(self.request.GET.get('month', None))
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+        return context
+
 def get_date(req_day):
     if req_day:
         year, month = (int(x) for x in req_day.split('-'))
         return date(year, month, day=1)
     return datetime.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('cal:calendar'))
+    return render(request, 'cal/event.html', {'form': form})
